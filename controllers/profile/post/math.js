ff477cc54b1dabcd33d11e815e5540c45e3d8d32
khudayormm <khudayorm@gmail.com>
2022-05-24 15:52:26 +0500

    sdfghm,

--- controllers/profile/post/math.js
+++ controllers/profile/post/math.js
@@ -1,5 +1,25 @@
-
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 module.exports = (req, res) => {
-    console.log(req.body)
+    const data = req.body
+    
+    const keys = Object.keys(data)
+    console.log(keys)
+ 
+    const values = Object.values(data)
+    console.log(values)
+
+
+    let user_answer = keys.map((item, id) => {
+        ua_id: id
+    })
+
+
+
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
 }  
